0cc8b36bd5bc18cd5905ed5dbbfa6271ef4ffd47
yara albouq <yaramalbouq@gmail.com>
2024-08-14 00:33:50 +0300

    second page

--- assignment/lib/src/second_screen.dart
+++ assignment/lib/src/second_screen.dart
@@ -50,7 +50,7 @@ class SecondScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(
               Icons.ios_share_rounded,
-              color:  Color.fromARGB(255, 203, 203, 203),
+              color: Color.fromARGB(255, 203, 203, 203),
             ),
             onPressed: () {},
           ),
@@ -58,10 +58,156 @@ class SecondScreen extends StatelessWidget {
         ],
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       ),
-      body: const Center(
-        child: Column(),
-      
-      ),
+      body: Center(
+          child: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const Icon(
+            Icons.my_library_books,
+            size: 40,
+          ),
+          const Text(
+            "Explain",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 20),
+              alignment: Alignment.center,
+              width: 300,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " Explain Quantum physics ",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.only(top: 10, bottom: 50),
+              alignment: Alignment.center,
+              width: 300,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " Best programming language ",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+          const Icon(
+            Icons.edit_outlined,
+            size: 40,
+          ),
+          const Text(
+            "Write & Edit",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 30),
+              alignment: Alignment.center,
+              width: 300,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " Write a tweet about global warming",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.only(
+                top: 10,
+              ),
+              alignment: Alignment.center,
+              width: 300,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " Write a poem about flower and love ",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.only(top: 10, bottom: 50),
+              alignment: Alignment.center,
+              width: 300,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " Write about a rap song lyrics ",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+          const Icon(
+            Icons.translate,
+            size: 40,
+          ),
+          const Text(
+            "Translate",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 20),
+              alignment: Alignment.center,
+              width: 380,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " How do you say (how are you) in korean?",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.only(
+                top: 10,
+              ),
+              alignment: Alignment.center,
+              width: 380,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " How do you say (how are you) in Arabic? ",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.only(
+                top: 10,
+              ),
+              alignment: Alignment.center,
+              width: 380,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Text(
+                " How do you say (how are you) in Spanish? ",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+        ],
+      )),
     );
   }
 }
