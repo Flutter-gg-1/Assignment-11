60faccc7b5aea1ff8fba8ffc4d39062a3a8a76da
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-13 21:54:25 +0300

    ADD Icons

--- lib/pages/page_2.dart
+++ lib/pages/page_2.dart
@@ -7,7 +7,38 @@ class ChatGPTPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChatGPT'),
+        leading: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Expanded(
+                      child: Text('ChatGPT',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color.fromRGBO(0, 86, 254, 1),
+                          ))),
+                  Expanded(
+                      child: Text('Online',
+                          style: TextStyle(color: Colors.green))),
+                ],
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              const Icon(Icons.volume_up),
+              const SizedBox(width: 10),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.file_upload_outlined),
+                  ),
+            ],
+          ),
+        ],
       ),
       body: const Center(
         child: Text('ChatGPT Page'),
