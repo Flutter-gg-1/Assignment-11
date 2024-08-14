b8b5f82cdeee62009bc294e9e60db644270bfeab
Linda <LindaAlmakramy@gmail.com>
2024-08-14 07:50:56 +0300

    added second page

--- assignment11/lib/explain_page.dart
+++ assignment11/lib/explain_page.dart
@@ -11,8 +11,8 @@ class ExplainPage extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
+            child: SingleChildScrollView(
             child: Column(
-              
               children: [
                AppBar(
                 leading: IconButton(
@@ -42,21 +42,209 @@ class ExplainPage extends StatelessWidget {
                 ),
                 actions: [
                   IconButton(
-                    icon: const Icon(Icons.mic),
+                    icon: const Icon(Icons.volume_up),
                     onPressed: () {
                       Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const Homepage()));
                     }, 
                   ),
                 ],
               ),
-                
-            ],
+              const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.article_outlined, color: Colors.black), 
+                SizedBox(width: 8),
+                Text("Explain"),
+              ],
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Explain Quantum physics",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,                  ),
+                ),
+              ),
+            ),
+           SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const Homepage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Best programming language",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                  ),
+                ),
+              ),
+              ),
+              const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.border_color_outlined, color: Colors.black), 
+                SizedBox(width: 8),
+                Text("Write & edit"),
+              ],
+            ),
+             SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Write a tweet about global warming",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                  ),
+                ),
+              ),
+              ),
+               SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Write a poem about flowers and love",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                  ),
+                ),
+              ),
               
+              ),
+               SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Write a rap song lyrics about",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                    ),
+                  ),
+                ), 
+               ),
+               const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.translate, color: Colors.black), 
+                SizedBox(width: 8),
+                Text("Translate"),
+              ],
+            ),
+              SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "How do you say 'how are you?' in korean",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                  ),
+                ),
+              ),
+            ),
+             SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Write a poem about flowers and love",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                  ),
+                ),
+              ),
+             ),
+              SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Write a poem about flowers and love",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                  ),
+                ),
+              ),
+              
+              ),
+               SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                ),
+                child: const Text(
+                  "Write a rap song lyrics about",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 42, 42, 42),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,  
+                    ),
+                  ),
+                ), 
+               ),
+            ], 
           ),
           )
         ) 
       )
-     
+     )
     );
   }
 
